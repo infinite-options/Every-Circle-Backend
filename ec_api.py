002d1088376c85df27dd7e6397597f017baca6bc
saumya4751 <saumyashah4750@gmail.com>
2025-01-29 09:23:00 -0800

    added lists GET API

--- ec_api.py
+++ ec_api.py
@@ -19,6 +19,7 @@ from user_profile import Profile
 from business import Business
 from ratings import Ratings
 from search import Search
+from lists import Lists
 
 # from jwtToken import JwtToken
 from functools import wraps
@@ -446,6 +447,7 @@ api.add_resource(Business, "/business", "/business/<string:uid>")
 api.add_resource(Ratings, "/ratings", "/ratings/<string:uid>")
 api.add_resource(Search, "/search/<string:user_id>")
 api.add_resource(Refer, "/refer-a-friend")
+api.add_resource(Lists, "/lists/<string:list_uid>")
 
 
 
