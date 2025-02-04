e57c21fc757df93e3e50a47a8bbd06a4d2d4f2c0
saumya4751 <saumyashah4750@gmail.com>
2025-02-03 21:19:30 -0800

    updated ratings API, now it can add business with all the data

--- search.py
+++ search.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 from data_ec import connect, uploadImage, s3, processImage
 from charges import Charges
+from rapidfuzz import process
 
 # query -> category
 # category -> business
@@ -15,7 +16,7 @@ from charges import Charges
 class Search(Resource):
     def get(self, profile_id):
         print("In Search GET")
-        search_category = request.args.get('category')
+        search_category = request.args.get('category', "").strip()
         
         if search_category is None:
             abort(400, description="category is required")
@@ -24,49 +25,62 @@ class Search(Resource):
         
         try:
             with connect() as db:
-                category_query_response = db.select('every_circle.category', where={'category_name': search_category})
-                if not category_query_response['result']:
+                all_category_query = db.select('every_circle.category')
+                all_categories = {category['sub_category']: category['category_uid'] for category in all_category_query['result']}
+
+                match = process.extractOne(search_category, all_categories.keys(), score_cutoff=70)
+
+                if match:
+                    matched_category = match[0]
+                    category_uid = all_categories[matched_category]
+                    print(matched_category, category_uid)
+                else:
                     response['message'] = 'Category not found'
                     response['code'] = 200
                     return response, 200
-                
-                category_uid = category_query_response['result'][0]['category_uid']
-                
+
+                # category_query_response = db.select('every_circle.category', where={'category_name': search_category})
+                # if not category_query_response['result']:
+                #     response['message'] = 'Category not found'
+                #     response['code'] = 200
+                #     return response, 200
+                # category_uid = category_query_response['result'][0]['category_uid']
+
                 rating_query = f'''
                                     WITH UserConnections AS (
                                         WITH RECURSIVE Referrals AS (
                                             -- Base case: Start from the given user_id
                                             SELECT 
-                                                profile_user_id AS user_id,
+                                                profile_uid AS user_id,
                                                 profile_referred_by_user_id,
                                                 0 AS degree, 
-                                                CAST(profile_user_id AS CHAR(300)) AS connection_path
+                                                CAST(profile_uid AS CHAR(300)) AS connection_path
                                             FROM profile
-                                            WHERE profile_user_id = '{profile_id}'
+                                            WHERE profile_uid = '{profile_id}'
 
                                             UNION ALL
 
                                             -- Forward expansion: Find users referred by the current user
                                             SELECT 
-                                                p.profile_user_id AS user_id,
+                                                p.profile_uid AS user_id,
                                                 p.profile_referred_by_user_id,
                                                 r.degree + 1 AS degree,
-                                                CONCAT(r.connection_path, ' -> ', p.profile_user_id) AS connection_path
+                                                CONCAT(r.connection_path, ' -> ', p.profile_uid) AS connection_path
                                             FROM profile p
                                             INNER JOIN Referrals r ON p.profile_referred_by_user_id = r.user_id
                                             WHERE r.degree < 3 
-                                            AND NOT POSITION(p.profile_user_id IN r.connection_path) > 0  -- Prevent revisiting users
+                                            AND NOT POSITION(p.profile_uid IN r.connection_path) > 0  -- Prevent revisiting users
 
                                             UNION ALL
 
                                             -- Backward expansion: Find the user who referred the current user
                                             SELECT 
                                                 p.profile_referred_by_user_id AS user_id,
-                                                p.profile_user_id AS profile_referred_by_user_id,
+                                                p.profile_uid AS profile_referred_by_user_id,
                                                 r.degree + 1 AS degree,
                                                 CONCAT(r.connection_path, ' -> ', p.profile_referred_by_user_id) AS connection_path -- Append correctly
                                             FROM profile p
-                                            INNER JOIN Referrals r ON p.profile_user_id = r.user_id
+                                            INNER JOIN Referrals r ON p.profile_uid = r.user_id
                                             WHERE r.degree < 3
                                             AND NOT POSITION(p.profile_referred_by_user_id IN r.connection_path) > 0  -- Prevent revisiting users
                                         )
@@ -86,7 +100,7 @@ class Search(Resource):
                                             u.connection_path
                                         FROM
                                             ratings r
-                                        INNER JOIN UserConnections u ON r.rating_user_id = u.user_id
+                                        INNER JOIN UserConnections u ON r.rating_profile_id = u.user_id
                                         WHERE
                                             r.rating_business_id IN (
                                                 SELECT business_uid
@@ -111,11 +125,90 @@ class Search(Resource):
                                     FROM RatingMatches rm
                                     LEFT JOIN every_circle.business b ON rm.rating_business_id = b.business_uid;
                             '''
+
+                # rating_query = f'''
+                #                     WITH UserConnections AS (
+                #                         WITH RECURSIVE Referrals AS (
+                #                             -- Base case: Start from the given user_id
+                #                             SELECT 
+                #                                 profile_user_id AS user_id,
+                #                                 profile_referred_by_user_id,
+                #                                 0 AS degree, 
+                #                                 CAST(profile_user_id AS CHAR(300)) AS connection_path
+                #                             FROM profile
+                #                             WHERE profile_user_id = '{profile_id}'
+
+                #                             UNION ALL
+
+                #                             -- Forward expansion: Find users referred by the current user
+                #                             SELECT 
+                #                                 p.profile_user_id AS user_id,
+                #                                 p.profile_referred_by_user_id,
+                #                                 r.degree + 1 AS degree,
+                #                                 CONCAT(r.connection_path, ' -> ', p.profile_user_id) AS connection_path
+                #                             FROM profile p
+                #                             INNER JOIN Referrals r ON p.profile_referred_by_user_id = r.user_id
+                #                             WHERE r.degree < 3 
+                #                             AND NOT POSITION(p.profile_user_id IN r.connection_path) > 0  -- Prevent revisiting users
+
+                #                             UNION ALL
+
+                #                             -- Backward expansion: Find the user who referred the current user
+                #                             SELECT 
+                #                                 p.profile_referred_by_user_id AS user_id,
+                #                                 p.profile_user_id AS profile_referred_by_user_id,
+                #                                 r.degree + 1 AS degree,
+                #                                 CONCAT(r.connection_path, ' -> ', p.profile_referred_by_user_id) AS connection_path -- Append correctly
+                #                             FROM profile p
+                #                             INNER JOIN Referrals r ON p.profile_user_id = r.user_id
+                #                             WHERE r.degree < 3
+                #                             AND NOT POSITION(p.profile_referred_by_user_id IN r.connection_path) > 0  -- Prevent revisiting users
+                #                         )
+                #                         -- Final selection of all users within 3 degrees of connection
+                #                         SELECT DISTINCT
+                #                             user_id,
+                #                             degree,
+                #                             connection_path
+                #                         FROM Referrals
+                #                         ORDER BY degree, connection_path
+                #                     ),
+                #                     RatingMatches AS (
+                #                         -- Match ratings based on UserConnections and business category
+                #                         SELECT
+                #                             r.*,
+                #                             u.degree,
+                #                             u.connection_path
+                #                         FROM
+                #                             ratings r
+                #                         INNER JOIN UserConnections u ON r.rating_user_id = u.user_id
+                #                         WHERE
+                #                             r.rating_business_id IN (
+                #                                 SELECT business_uid
+                #                                 FROM every_circle.business
+                #                                 WHERE business_category_id = '{category_uid}'
+                #                             )
+                #                     )
+                #                     -- Final selection from RatingMatches to get the required output
+                #                     -- SELECT * 
+                #                     -- FROM RatingMatches;
+                #                     SELECT 
+                #                         rm.*, 
+                #                         b.business_name,
+                #                         b.business_phone_number,
+                #                         b.business_address_line_1,
+                #                         b.business_address_line_2,
+                #                         b.business_city,
+                #                         b.business_state,
+                #                         b.business_country,
+                #                         b.business_zip_code,
+                #                         b.business_google_id
+                #                     FROM RatingMatches rm
+                #                     LEFT JOIN every_circle.business b ON rm.rating_business_id = b.business_uid;
+                #             '''
                 
                 rating_query_response = db.execute(rating_query)
 
                 business_uid_list = tuple(set([business['rating_business_id'] for business in rating_query_response['result']]))
-                print(business_uid_list)
 
                 for business_uid in business_uid_list:
 
