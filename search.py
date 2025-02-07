71b9c9b5c510a60e592b78ce36f6af4b8e9cd3a1
saumya4751 <saumyashah4750@gmail.com>
2025-02-07 11:32:58 -0800

    fixed error in search

--- search.py
+++ search.py
@@ -240,7 +240,7 @@ class Search(Resource):
         
         try:
             with connect() as db:
-                all_type_query = db.select('every_circle.type')
+                all_type_query = db.select('every_circle.types')
                 all_types = {type['sub_type']: type['type_uid'] for type in all_type_query['result']}
 
                 match = process.extractOne(search_type, all_types.keys(), score_cutoff=70)
@@ -342,86 +342,6 @@ class Search(Resource):
                                     LEFT JOIN every_circle.business b ON rm.rating_business_id = b.business_uid;
                             '''
 
-                # rating_query = f'''
-                #                     WITH UserConnections AS (
-                #                         WITH RECURSIVE Referrals AS (
-                #                             -- Base case: Start from the given user_id
-                #                             SELECT 
-                #                                 profile_user_id AS user_id,
-                #                                 profile_referred_by_user_id,
-                #                                 0 AS degree, 
-                #                                 CAST(profile_user_id AS CHAR(300)) AS connection_path
-                #                             FROM profile
-                #                             WHERE profile_user_id = '{profile_id}'
-
-                #                             UNION ALL
-
-                #                             -- Forward expansion: Find users referred by the current user
-                #                             SELECT 
-                #                                 p.profile_user_id AS user_id,
-                #                                 p.profile_referred_by_user_id,
-                #                                 r.degree + 1 AS degree,
-                #                                 CONCAT(r.connection_path, ' -> ', p.profile_user_id) AS connection_path
-                #                             FROM profile p
-                #                             INNER JOIN Referrals r ON p.profile_referred_by_user_id = r.user_id
-                #                             WHERE r.degree < 3 
-                #                             AND NOT POSITION(p.profile_user_id IN r.connection_path) > 0  -- Prevent revisiting users
-
-                #                             UNION ALL
-
-                #                             -- Backward expansion: Find the user who referred the current user
-                #                             SELECT 
-                #                                 p.profile_referred_by_user_id AS user_id,
-                #                                 p.profile_user_id AS profile_referred_by_user_id,
-                #                                 r.degree + 1 AS degree,
-                #                                 CONCAT(r.connection_path, ' -> ', p.profile_referred_by_user_id) AS connection_path -- Append correctly
-                #                             FROM profile p
-                #                             INNER JOIN Referrals r ON p.profile_user_id = r.user_id
-                #                             WHERE r.degree < 3
-                #                             AND NOT POSITION(p.profile_referred_by_user_id IN r.connection_path) > 0  -- Prevent revisiting users
-                #                         )
-                #                         -- Final selection of all users within 3 degrees of connection
-                #                         SELECT DISTINCT
-                #                             user_id,
-                #                             degree,
-                #                             connection_path
-                #                         FROM Referrals
-                #                         ORDER BY degree, connection_path
-                #                     ),
-                #                     RatingMatches AS (
-                #                         -- Match ratings based on UserConnections and business type
-                #                         SELECT
-                #                             r.*,
-                #                             u.degree,
-                #                             u.connection_path
-                #                         FROM
-                #                             ratings r
-                #                         INNER JOIN UserConnections u ON r.rating_user_id = u.user_id
-                #                         WHERE
-                #                             r.rating_business_id IN (
-                #                                 SELECT business_uid
-                #                                 FROM every_circle.business
-                #                                 WHERE business_type_id = '{type_uid}'
-                #                             )
-                #                     )
-                #                     -- Final selection from RatingMatches to get the required output
-                #                     -- SELECT * 
-                #                     -- FROM RatingMatches;
-                #                     SELECT 
-                #                         rm.*, 
-                #                         b.business_name,
-                #                         b.business_phone_number,
-                #                         b.business_address_line_1,
-                #                         b.business_address_line_2,
-                #                         b.business_city,
-                #                         b.business_state,
-                #                         b.business_country,
-                #                         b.business_zip_code,
-                #                         b.business_google_id
-                #                     FROM RatingMatches rm
-                #                     LEFT JOIN every_circle.business b ON rm.rating_business_id = b.business_uid;
-                #             '''
-                
                 rating_query_response = db.execute(rating_query)
 
                 business_uid_list = tuple(set([business['rating_business_id'] for business in rating_query_response['result']]))
@@ -450,12 +370,12 @@ class Search(Resource):
 
 '''
 -- b.business_name,
-                                        -- b.business_phone_number,
-                                        -- b.business_address_line_1,
-                                        -- b.business_address_line_2,
-                                        -- b.business_city,
-                                        -- b.business_state,
-                                        -- b.business_country,
-                                        -- b.business_zip_code,
-                                        -- b.business_google_id
+-- b.business_phone_number,
+-- b.business_address_line_1,
+-- b.business_address_line_2,
+-- b.business_city,
+-- b.business_state,
+-- b.business_country,
+-- b.business_zip_code,
+-- b.business_google_id
 '''
