4f66225be2056eb2925ec1fa10097c9bbc0fca02
shruti03-design <shrutisen03@gmail.com>
2026-05-04 10:15:27 -0700

    added decline

--- ec_api.py
+++ ec_api.py
@@ -32,7 +32,7 @@ from chatbot import ChatbotAPI
 from tag_generator_api import TagGeneratorAPI
 from user_profile_info import UserProfileInfo
 from business_info import BusinessInfo
-from transactions import Transactions, SellerTransactions
+from transactions import Transactions, SellerTransactions, DeclinedReturns
 from user_path_connection import ConnectionsPath
 from network_connection import NetworkPath
 from profile_details import ProfileDetails
@@ -554,6 +554,7 @@ api.add_resource(SearchReferral, '/api/search_referral')
 api.add_resource(BusinessAvgRatings, '/api/v1/businessavgratings')
 api.add_resource(BusinessTagSearch, '/api/v1/businesstagsearch')
 api.add_resource(AblyToken, '/api/v1/ably/token')
+api.add_resource(DeclinedReturns, '/api/v1/transactions/returns/declined')
 
 class GooglePlacesInfo(Resource):
     def post(self):
