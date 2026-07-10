ef20df4aeff8cce33f169ff0475800c6ccc52f02
shruti03-design <shrutisen03@gmail.com>
2026-07-10 09:59:14 -0700

    replaced connections_path to use profile_personal_path

--- user_path_connection.py
+++ user_path_connection.py
@@ -11,79 +11,107 @@ class ConnectionsPath(Resource):
         print("second_uid: ", second_uid)
         with connect() as db:
 
-            first_connection_query = f''' WITH RECURSIVE ReferralPath AS (
-                                    SELECT 
-                                        profile_personal_uid AS user_id,
-                                        profile_personal_referred_by,
-                                        CAST(profile_personal_uid AS CHAR(255)) AS path
-                                    FROM profile_personal
-                                    WHERE profile_personal_uid = '110-000001'
-
-                                    UNION ALL
-
-                                    SELECT 
-                                        p.profile_personal_uid,
-                                        p.profile_personal_referred_by,
-                                        CONCAT(r.path, '->', p.profile_personal_uid)
-                                    FROM profile_personal p
-                                    JOIN ReferralPath r ON p.profile_personal_referred_by = r.user_id
-                                    WHERE LOCATE(p.profile_personal_uid, r.path) = 0 
-                                )
-
-                                SELECT *
-                                FROM ReferralPath
-                                WHERE user_id = '{first_uid}';
+            # Try the precomputed profile_personal_path first (fast, indexed lookup)
+            first_stored_query = f''' SELECT profile_personal_path AS path
+                                FROM profile_personal
+                                WHERE profile_personal_uid = '{first_uid}';
                         '''
-            
 
-            response = db.execute(first_connection_query)
+            response = db.execute(first_stored_query)
             print(response)
 
-            if not response['result']:
-                response['message'] = 'No connection found'
-                response['code'] = 404
-                return response, 404
+            if response['result'] and response['result'][0]['path']:
+                stored_path = response['result'][0]['path']
+                first_user_details = {'path': '->'.join(uid.strip().strip("'") for uid in stored_path.split(','))}
+            else:
+                # profile_personal_path not available - fall back to recomputing it
+                first_connection_query = f''' WITH RECURSIVE ReferralPath AS (
+                                        SELECT
+                                            profile_personal_uid AS user_id,
+                                            profile_personal_referred_by,
+                                            CAST(profile_personal_uid AS CHAR(255)) AS path
+                                        FROM profile_personal
+                                        WHERE profile_personal_uid = '110-000001'
 
-            first_user_details = response['result'][0]
+                                        UNION ALL
+
+                                        SELECT
+                                            p.profile_personal_uid,
+                                            p.profile_personal_referred_by,
+                                            CONCAT(r.path, '->', p.profile_personal_uid)
+                                        FROM profile_personal p
+                                        JOIN ReferralPath r ON p.profile_personal_referred_by = r.user_id
+                                        WHERE LOCATE(p.profile_personal_uid, r.path) = 0
+                                    )
+
+                                    SELECT *
+                                    FROM ReferralPath
+                                    WHERE user_id = '{first_uid}';
+                            '''
+
+                response = db.execute(first_connection_query)
+                print(response)
+
+                if not response['result']:
+                    response['message'] = 'No connection found'
+                    response['code'] = 404
+                    return response, 404
+
+                first_user_details = response['result'][0]
+
             print('first_user_details: ', first_user_details)
 
-            second_connection_query = f''' WITH RECURSIVE ReferralPath AS (
-                                    SELECT 
-                                        profile_personal_uid AS user_id,
-                                        profile_personal_referred_by,
-                                        CAST(profile_personal_uid AS CHAR(255)) AS path
-                                    FROM profile_personal
-                                    WHERE profile_personal_uid = '110-000001'
-
-                                    UNION ALL
-
-                                    SELECT 
-                                        p.profile_personal_uid,
-                                        p.profile_personal_referred_by,
-                                        CONCAT(r.path, '->', p.profile_personal_uid)
-                                    FROM profile_personal p
-                                    JOIN ReferralPath r ON p.profile_personal_referred_by = r.user_id
-                                    WHERE LOCATE(p.profile_personal_uid, r.path) = 0 
-                                )
-
-                                SELECT *
-                                FROM ReferralPath
-                                WHERE user_id = '{second_uid}';
+            # Try the precomputed profile_personal_path first (fast, indexed lookup)
+            second_stored_query = f''' SELECT profile_personal_path AS path
+                                FROM profile_personal
+                                WHERE profile_personal_uid = '{second_uid}';
                         '''
 
-
-            response = db.execute(second_connection_query)
+            response = db.execute(second_stored_query)
             print(response)
 
-            if not response['result']:
-                response['message'] = 'No connection found'
-                response['code'] = 404
-                return response, 404
+            if response['result'] and response['result'][0]['path']:
+                stored_path = response['result'][0]['path']
+                second_user_details = {'path': '->'.join(uid.strip().strip("'") for uid in stored_path.split(','))}
+            else:
+                # profile_personal_path not available - fall back to recomputing it
+                second_connection_query = f''' WITH RECURSIVE ReferralPath AS (
+                                        SELECT
+                                            profile_personal_uid AS user_id,
+                                            profile_personal_referred_by,
+                                            CAST(profile_personal_uid AS CHAR(255)) AS path
+                                        FROM profile_personal
+                                        WHERE profile_personal_uid = '110-000001'
 
-            second_user_details = response['result'][0]
+                                        UNION ALL
+
+                                        SELECT
+                                            p.profile_personal_uid,
+                                            p.profile_personal_referred_by,
+                                            CONCAT(r.path, '->', p.profile_personal_uid)
+                                        FROM profile_personal p
+                                        JOIN ReferralPath r ON p.profile_personal_referred_by = r.user_id
+                                        WHERE LOCATE(p.profile_personal_uid, r.path) = 0
+                                    )
+
+                                    SELECT *
+                                    FROM ReferralPath
+                                    WHERE user_id = '{second_uid}';
+                            '''
+
+                response = db.execute(second_connection_query)
+                print(response)
+
+                if not response['result']:
+                    response['message'] = 'No connection found'
+                    response['code'] = 404
+                    return response, 404
+
+                second_user_details = response['result'][0]
+
             print('second_user_details', second_user_details)
 
-            
+
             #concatenation of the combined results
             def combine_paths(first_path, second_path):
                 first_list = first_path.split('->')
@@ -121,13 +149,13 @@ class ConnectionsPath(Resource):
                 # else:
 
                 if first_list[-1] != lca:  # first_list is the start
-                    
+
                     reversed_suffix = after_common_1[::-1]
                     other_suffix = after_common_2
                 else:  # second_list is the start
-                    reversed_suffix = after_common_2[::-1] 
+                    reversed_suffix = after_common_2[::-1]
                     other_suffix = after_common_1
-                    
+
 
                 # combined_path = after_common_1 +[lca]+ after_common_2[::-1]
                 print('reversed_suffix', reversed_suffix, 'lca', lca, 'other_suffix', other_suffix)
@@ -135,12 +163,14 @@ class ConnectionsPath(Resource):
 
                 return ','.join(combined_path)
 
-            
+
             combined = combine_paths(first_user_details['path'],
                                      second_user_details['path'])
-            
+
             print("Combined path:", combined)
 
             return {'combined_path': combined}, 200
 
             # return jsonify({'combined':combined}), 200
+
+
