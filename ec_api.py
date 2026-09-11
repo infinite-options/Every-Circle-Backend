b05bb10f5517fa9b28029aba9cae86956fbcabeb
pmarathay <pmarathay@infiniteoptions.com>
2026-09-11 10:12:28 -0700

    added print statement

--- ec_api.py
+++ ec_api.py
@@ -404,7 +404,7 @@ else:
 # -- Send Email Endpoints start here -------------------------------------------------------------------------------
 
 def sendEmail(recipient, subject, body):
-    print('in sendEmail')
+    print('in sendEmail function')
     print('Confirming correct function call')
     print('recipient received', recipient)
     # Flask-Mail Message.recipients must be a list; if passed a string it iterates per-character.
@@ -445,6 +445,7 @@ class SendEmail(Resource):
             #     "Thank you - Nitya Ayurveda\n\n"
             # )
             sendEmail([email], "Thanks for your Note!", body)
+            print("In Send EMail post after sendEmail")
             return "Email Sent", 200
 
         except Exception:
